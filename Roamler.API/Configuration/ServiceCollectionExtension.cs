8600612521bbf23e10073e1dbf70444922d9101d
Mojtaba Tajik <onyfel@gmail.com>
2023-02-22 13:12:23 +0330

    - Add Swagger documentation

--- Roamler.API/Configuration/ServiceCollectionExtension.cs
+++ Roamler.API/Configuration/ServiceCollectionExtension.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace Roamler.API.Configuration;
 
 public static class ServiceCollectionExtension
@@ -7,6 +9,29 @@ public static class ServiceCollectionExtension
         services.AddControllers();
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
+        services.AddSwaggerDocumentation();
         services.AddHealthChecks();
+    }
+    
+    private static void AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc(Consts.Swagger.ApiVersion, new()
+            {
+                Version = Consts.Swagger.ApiVersion,
+                Title = Consts.Swagger.Title,
+                Description = Consts.Swagger.Description,
+                TermsOfService = new(Consts.Swagger.TermsOfService),
+                Contact = new()
+                {
+                    Name = Consts.Swagger.ContactTitle,
+                    Email = Consts.Swagger.ContactEmailAddress
+                },
+            });
+    
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
     }
 }
---
+++ Roamler.API/Consts.cs
@@ -0,0 +1,14 @@
+namespace Roamler.API;
+
+public class Consts
+{
+    public static class Swagger
+    {
+        public const string Title = "Roamler Location API";
+        public const string Description = "Web API to interact with location data of Roamler";
+        public const string TermsOfService = "https://www.roamler.com/roamlers/support/faq/";
+        public const string ApiVersion = "v1";
+        public const string ContactTitle = "Support";
+        public const string ContactEmailAddress = "mojtaba@roamler.com";
+    }
+}
