c2fe083bcbe60b29dd82f02e46a4b4dc8b83df2f
Mojtaba Tajik <onyfel@gmail.com>
2023-02-22 11:59:29 +0330

    - Add LocationController - Implement AddLocation [PUT] endpoint

--- Roamler.API/Controllers/Location/LocationController.cs
+++ Roamler.API/Controllers/Location/LocationController.cs
@@ -1,4 +1,7 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Roamler.Application.DTO;
+using Roamler.Application.Location.Commands.AddLocation;
 
 namespace Roamler.API.Controllers.Location;
 
@@ -6,5 +9,19 @@ namespace Roamler.API.Controllers.Location;
 [Route("[controller]")]
 public class LocationController : ControllerBase
 {
+    private readonly ISender _sender;
 
+    public LocationController(ISender sender)
+    {
+        _sender = sender;
+    }
+    
+    [HttpPut("AddLocation")]
+    public async Task<IActionResult> AddLocation(LocationWithAddress loc)
+    {
+        var addLocationCommand = new AddLocationCommand(loc);
+        var addResult = await _sender.Send(addLocationCommand);
+        
+        return Ok(addResult);
+    }
 }
