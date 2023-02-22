d97d6452cdb4ea54a05f9120de5599fe5dcae67e
Mojtaba Tajik <onyfel@gmail.com>
2023-02-22 12:24:53 +0330

    Add AddLocationsFromCsvFile to read CSV location file and add it's data to location store

--- Roamler.API/Controllers/Location/LocationController.cs
+++ Roamler.API/Controllers/Location/LocationController.cs
@@ -1,7 +1,9 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Roamler.Application.CsvFile.Queries.ReadFileToLocation;
 using Roamler.Application.DTO;
 using Roamler.Application.Location.Commands.AddLocation;
+using Roamler.Application.Location.Commands.AddLocationRange;
 
 namespace Roamler.API.Controllers.Location;
 
@@ -23,5 +25,23 @@ public class LocationController : ControllerBase
         var addResult = await _sender.Send(addLocationCommand);
         
         return Ok(addResult);
+    }
+    
+    [HttpPost("AddLocationsFromCsv")]
+    public async Task<IActionResult> AddLocationsFromCsvFile(IFormFile csvFile)
+    {
+        var readCsvLocationsCommand = new ReadFileToLocationQuery(csvFile.OpenReadStream());
+        var locations = await _sender.Send(readCsvLocationsCommand);
+        if (locations.IsFailed)
+        {
+            return BadRequest();
+        }
+
+        var addLocationRangeCommand = new AddLocationRangeCommand(locations.Data);
+        var addResult = await _sender.Send(addLocationRangeCommand);
+        
+        return addResult.IsSuccess
+            ? Ok() 
+            : BadRequest();
     }
 }
