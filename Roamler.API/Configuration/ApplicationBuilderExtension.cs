f3f79763a05e5b398f6b40b4f23511a4b9c2babb
Mojtaba Tajik <onyfel@gmail.com>
2023-02-22 15:16:37 +0330

    Show Swagger in production too

--- Roamler.API/Configuration/ApplicationBuilderExtension.cs
+++ Roamler.API/Configuration/ApplicationBuilderExtension.cs
@@ -4,10 +4,7 @@ public static class ApplicationBuilderExtension
 {
     public static void AddSwagger(this IApplicationBuilder app, bool isDevelopment)
     {
-        if (isDevelopment == true)
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
 }
