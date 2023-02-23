fce03e9f4f86b07f7ca984902fd4f2458d93a934
Mojtaba Tajik <onyfel@gmail.com>
2023-02-23 09:08:29 +0330

    Write health check endpoint integration test

--- IntegrationTests/IntegrationTest.cs
+++ IntegrationTests/IntegrationTest.cs
@@ -1,8 +1,32 @@
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Web;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Roamler.Application.DTO;
 
 namespace IntegrationTests;
 
 public class IntegrationTest : IClassFixture<WebApplicationFactory<Program>>
 {
+    private readonly HttpClient? _client;
     
+    public IntegrationTest(WebApplicationFactory<Program> factory)
+    {
+        Environment.SetEnvironmentVariable("RoamlerGeoRedisDB", "localhost:6381");
+        
+        _client = factory.CreateClient();
+    }
+
+    [Theory]
+    [InlineData("/health")]
+    public async Task Should_return_success_for_static_urls(string url)
+    {
+        // Act
+        var response = await _client.GetAsync(url);
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+    }
 }
